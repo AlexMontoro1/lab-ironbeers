676862fa39994a4a66c56e9dec20b8fbb9288763
Alejandro Montoro <alextermontoro@hotmail.com>
2023-05-03 23:35:12 +0200

    solved lab

--- app.js
+++ app.js
@@ -12,14 +12,59 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(response => {
+      console.log(response);
+
+      res.render('beers.hbs', {
+        allBeers: response
+      });
+    })
+    .catch(error => {
+      //console.log(error);
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(response => {
+      res.render('random-beer.hbs', {
+        randomBeer: response
+      });
+    })
+    .catch(error => {});
+});
+
+app.get('/beers/:id', (req, res) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then(response => {
+      res.render('beer-info.hbs', {
+        beer: response[0]
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// PASOS PARA CREAR RUTAS
+
+// 1. CREAR LA RUTA SIN NADA ADENTRO
+// 2. CREAMOS UNA VISTA CON UNA ESTRUCTURA BASE (SIN CONTENIDO)
+// 3. RENDERIZAMOS ESA VISTA EN LA RUTA Y LA PROBAMOS
+// 4. BUSCAR LA DATA Y PASARLA A LA VISTA
+// 5. RENDERIZAR LA DARA EN EL HBS
